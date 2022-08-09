044b16c4db057419ac1449ec60b581bc59dec907
Joydeep Mukherjee <joydeepm02@gmail.com>
2022-08-09 00:48:05 -0400

    Add User and Course models

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.user_id
+
+class Course(db.Model):
+    course_id = db.Column(db.Integer, primary_key=True)
+    department = db.Column(db.String(20))
+    number = db.Column(db.Integer)
+    name = db.Column(db.String(20))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"))
+
+    def __repr__(self):
+        return "<Course %r>" % self.course_id
 
 @app.route("/")
 def hello_world():
